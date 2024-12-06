49f3b38644c148637a8ff9a4f0a9765f664dfeaa
Bypasser <127021221+someonelearn@users.noreply.github.com>
2024-12-06 10:10:07 +0700

    Update and rename loen_.py to cal_energy_.py

---
+++ cal_energy_.py
@@ -0,0 +1,9 @@
+import torch
+from PIL import Image
+from NoiseAddition import GaussianNoise
+
+def cal_energy_score(latent):
+    #x.shape = (bsize, score)
+    y_pred = latent[0].argmax(dim=-1, keepdim=True)
+    Energy = -latent[:, y_pred]
+    return torch.mean(Energy, dim=0, keepdim=True)
